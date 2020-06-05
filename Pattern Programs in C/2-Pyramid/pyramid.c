244cd1595db169d5066e12cd0dcff21957079ea9
anuanurag2410 <38002218+anuanurag2410@users.noreply.github.com>
2020-06-05 16:56:35 +0530

    Update pyramid.c

--- Pattern Programs in C/2-Pyramid/pyramid.c
+++ Pattern Programs in C/2-Pyramid/pyramid.c
@@ -1,22 +1,29 @@
-#include<conio.h>
+ #include<conio.h>
 #include<stdio.h>
 
 void main()
 {
+int n,i,j,space;
 
-for(int i=1;i<=10;i++)
+printf("Enter the number of rows\n");
+scanf("%d",&n);
+
+for(i=1;i<=n;i++) //row loop
 {
-    for(int space=10-i;space>=1;space--)
-    {
-        printf(" ");
-    }
-    for(int j=1;j<=2*i-1;j++)
-    {
-        printf("*"); 
-    }
-    printf("\n");
+for(space=n-i;space>=1;space--)//space loop
+{
+    printf(" ");
+}
+for(j=1;j<=2*i-1;j++)//star printing loop
+{
+    printf("*");
+}
+printf("\n");
 
 }
+
+
+
     getch();
 
 }
